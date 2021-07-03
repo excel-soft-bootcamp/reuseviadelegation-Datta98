225001bc080ae86cb134a8af5bb1485717109ffc
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-07-03 13:09:12 +0530

    Add files via upload

---
+++ SmartPhoneApplication/IPhone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPhoneApplication
+{
+    public interface IPhone
+    {
+        void Hangup();
+        void Release();
+    }
+}
---
+++ SmartPhoneApplication/Photographer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartPhoneApplication
+{
+    public class Photographer
+    {
+        public void Capture(ICamera camera)
+        {
+            camera.TakePicture();
+        }
+    }
+}
